75321f94bf6b2d20022ca8858ed6c304f89a2891
kgarcia <kgarcia@scl.edu.gt>
2024-05-29 20:30:54 -0600

    TodoList

--- js/data_todo.js
+++ js/data_todo.js
@@ -9,16 +9,6 @@ let tareas = [
         id: 2,
         texto: "levantar la tapa con cuidado",
     },
-    {
-        estado: true,
-        id: 3,
-        texto: "Desconectar la batería",
-    },
-    {
-        estado: false,
-        id: 4,
-        texto: "Identificar componentes",
-    }
 ]
 
 export{tareas};
